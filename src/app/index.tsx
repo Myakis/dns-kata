767b4f63556577b4b64e4d2a10d5b07eba8d2634
Darya Kasyanova <daskas007@gmail.com>
2024-04-25 18:34:19 +0700

    общий layout

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,19 @@
+import { Route, Routes, useLocation } from 'react-router-dom';
+import { Layout } from 'pages/layouts/Layout';
+import { getPageTitle } from 'shared/utils';
+import './reset.scss';
+
 const App = () => {
+  const location = useLocation();
+  const pageTitle = getPageTitle(location.pathname);
 
   return (
-    <>
-      <h1>hello wrold</h1>
-    </>
-  )
-}
+    <Routes>
+      <Route path='/' element={<Layout pageTitle={pageTitle} breadcrumbs={['Главная']} />}>
+        <Route path='news' element={<div>News</div>}></Route>
+      </Route>
+    </Routes>
+  );
+};
 
-export default App
+export default App;
---
+++ src/pages/layouts/Layout/Layout.tsx
@@ -0,0 +1,35 @@
+import { Outlet } from 'react-router-dom';
+import styles from './Layout.module.scss';
+
+interface TLayout {
+  pageTitle: string | undefined;
+  breadcrumbs: any[];
+}
+
+export const Layout: React.FC<TLayout> = ({ pageTitle, breadcrumbs }) => {
+  return (
+    <div className={styles.layout}>
+      <header className={styles.header}>
+        <div className={styles.header_container}>Header</div> {/*Сюда вставляем header*/}
+      </header>
+      <div className={styles.breadcrumbs}>
+        <nav>
+          <ul>
+            {breadcrumbs.map((breadcrumb: string, index: number) => (
+              <li key={index}>
+                <a>{breadcrumb}</a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
+      <main className={styles.content}>
+        <h1 className={styles.page_title}>{pageTitle}</h1>
+        <Outlet />
+      </main>
+      <footer className={styles.footer}>
+        <div className={styles.footer_container}>Footer</div> {/*Сюда вставляем footer*/}
+      </footer>
+    </div>
+  );
+};
