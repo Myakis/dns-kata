c35fd4487f430e8b91ec65d11ee84ed6b55c687c
Darya Kasyanova <daskas007@gmail.com>
2024-04-26 20:06:23 +0700

    fixed header

--- src/pages/layouts/Header/Header.tsx
+++ src/pages/layouts/Header/Header.tsx
@@ -1,14 +1,34 @@
 import styles from './Header.module.scss';
+import { useEffect } from 'react';
 
 export const Header: React.FC = () => {
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const handleScroll = () => {
+    const mainHeader = document.querySelector(`.${styles['main-header']}`);
+    const mainHeaderContainer = document.querySelector(`.${styles['main-header__container']}`);
+    if (mainHeader && window.scrollY > 0) {
+      mainHeader.classList.add(styles['main-header--fixed']);
+      mainHeaderContainer?.classList.add(styles['main-header__container--fixed']);
+    } else {
+      mainHeader?.classList.remove(styles['main-header--fixed']);
+      mainHeaderContainer?.classList.remove(styles['main-header__container--fixed']);
+    }
+  };
+
   return (
     <div className={styles.header}>
-      <div className={styles.upper_header}>
-        <div className={styles.location}>
+      <div className={styles['upper-header']}>
+        <div className={styles['upper-header__location']}>
           <a>Москва</a>
         </div>
         <nav>
-          <ul className={styles.header_navigation}>
+          <ul className={styles['upper-header__navigation']}>
             <li>
               <a href=''>Акции</a>
             </li>
@@ -29,35 +49,37 @@ export const Header: React.FC = () => {
             </li>
           </ul>
         </nav>
-        <div className={styles.tel}>
+        <div className={styles['upper-header__tel']}>
           <a href='tel:8-800-77-07-999'>8-800-77-07-999</a>
         </div>
       </div>
-      <div className={styles.main_header}>
-        <div className={`${styles.orange_btn} ${styles.header_btn_bg}`}>
-          <a className={styles.logo_btn}></a>
-          <button className={styles.catalog_btn}>Каталог</button>
+      <div className={styles['main-header']}>
+        <div className={styles['main-header__container']}>
+          <div className={`${styles['main-header__orange-btn']} ${styles['main-header__btn-bg']}`}>
+            <a className={styles['main-header__logo-btn']}></a>
+            <button className={styles['main-header__catalog-btn']}>Каталог</button>
+          </div>
+          <div className={styles['main-header__search-wrapper']}>
+            <input type='text' className={styles['main-header__search']} placeholder='Поиск по сайту' />
+            <button className={styles['main-header__search-btn']}></button>
+          </div>
+          <nav>
+            <ul className={styles['main-header__side-nav']}>
+              <li className={styles['side-nav__compare']}>
+                <a href=''>Сравнение</a>
+              </li>
+              <li className={styles['side-nav__favorited']}>
+                <a href=''>Избранное</a>
+              </li>
+              <li className={styles['side-nav__basket']}>
+                <a href=''>Корзина</a>
+              </li>
+              <li className={styles['side-nav__log-in']}>
+                <a href=''>Войти</a>
+              </li>
+            </ul>
+          </nav>
         </div>
-        <div className={styles.search_wrapper}>
-          <input type='text' className={styles.search} placeholder='Поиск по сайту' />
-          <button className={styles.search_btn}></button>
-        </div>
-        <nav>
-          <ul className={styles.side_nav}>
-            <li className={styles.compare}>
-              <a href=''>Сравнение</a>
-            </li>
-            <li className={styles.favorited}>
-              <a href=''>Избранное</a>
-            </li>
-            <li className={styles.basket}>
-              <a href=''>Корзина</a>
-            </li>
-            <li className={styles.log_in}>
-              <a href=''>Войти</a>
-            </li>
-          </ul>
-        </nav>
       </div>
     </div>
   );
