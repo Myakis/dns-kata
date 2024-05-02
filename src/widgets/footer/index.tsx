932f3929d065f4577de00239eba613d3a70e218e
Tsurkan Georgiy <egorizz@mail.ru>
2024-05-02 12:54:44 +0500

    Вынес переменные с ссылками в отдельный файл, вынес дополнительные функции за компонент

--- src/widgets/footer/index.tsx
+++ src/widgets/footer/index.tsx
@@ -1,68 +1,32 @@
 import React, { useState, useEffect } from 'react';
+import { companyLinks, customerLinks, appLinks, Link } from './constants/constants';
 import styles from './styles.module.scss';
 
-interface Link {
-  title: string;
-  href: string;
-}
-
 interface Props {}
+
+const renderLinks = (links: Link[]) => {
+  return links.map((link: Link, index: number) => (
+    <li key={index} className={styles.navmenu__linkWrapper}>
+      <a href={link.href} className={styles.navmenu__submenuLink}>
+        {link.title}
+      </a>
+    </li>
+  ));
+};
+
+const renderMobileLinks = (links: Link[]) => {
+  return links.map((link: Link, index: number) => (
+    <li key={index} className={styles.mobileMenu__linkWrapper}>
+      <a href={link.href} className={styles.mobileMenu__submenuLink}>
+        {link.title}
+      </a>
+    </li>
+  ));
+};
 
 const Footer: React.FC<Props> = () => {
   const [selectedSection, setSelectedSection] = useState<string | null>(null);
   const [prevSection, setPrevSection] = useState<string | null>(null);
-
-  const companyLinks: Link[] = [
-    { title: 'О Компании', href: 'https://www.dns-shop.ru/about' },
-    { title: 'Новости', href: 'https://www.dns-shop.ru/news/' },
-    { title: 'Партнерам', href: 'https://www.dns-shop.ru/about/partners' },
-    { title: 'Вакансии', href: 'https://www.dns-shop.ru/about/career' },
-    { title: 'Политика конфеденциальности', href: 'https://www.dns-shop.ru/rules/policy/#policy-one' },
-    { title: 'Персональные данные', href: 'https://www.dns-shop.ru/rules/personal-data/#personal-data-intro' },
-    { title: 'Правила продаж', href: 'https://www.dns-shop.ru/rules/#rule-one' },
-    { title: 'Правила пользования сайта', href: 'https://www.dns-shop.ru/rules/site-usage/#usage-one' },
-    {
-      title: 'На информационном ресурсе применяются рекомендательные технологии',
-      href: 'https://www.dns-shop.ru/rules/recommend-tech/#recommend-tech-one',
-    },
-    { title: 'Сервисные центры', href: 'https://www.dns-shop.ru/service-center/' },
-  ];
-
-  const customerLinks: Link[] = [
-    {
-      title: 'Как оформить заказ',
-      href: 'https://www.dns-shop.ru/help/useful-information/8145d90b-1c3a-44c4-84b9-bb3f54aa783a/',
-    },
-    {
-      title: 'Способы оплаты',
-      href: 'https://www.dns-shop.ru/help/useful-information/f112d810-a586-4c60-b651-8d328fe0e0e7/',
-    },
-    { title: 'Кредиты', href: 'https://www.dns-shop.ru/credit/' },
-    { title: 'Доставка', href: 'https://www.dns-shop.ru/help/delivery/' },
-    { title: 'Статус заказа', href: 'https://www.dns-shop.ru/profile/order/all/' },
-    { title: 'Обмен, возврат, гарантия', href: 'https://www.dns-shop.ru/service-center/exchange-and-returns/' },
-    { title: 'Проверка статуса ремонта', href: 'https://www.dns-shop.ru/service-center/status/' },
-    { title: 'Юридическим лицам', href: 'https://www.dns-shop.ru/help/legal-entities/' },
-    { title: 'Проверка счета', href: 'https://www.dns-shop.ru/help/check/' },
-    { title: 'Корпоративные отделы', href: 'https://www.dns-shop.ru/help/legal-entities/#corporate-shops-list' },
-    { title: 'Подарочные карты', href: 'https://www.dns-shop.ru/gift-card/' },
-    { title: 'Бонусная программа', href: 'https://prozapass.ru/' },
-    { title: 'Помощь', href: 'https://www.dns-shop.ru/help/' },
-    { title: 'Обратная связь', href: 'https://www.dns-shop.ru/feedback/' },
-  ];
-
-  const appLinks = [
-    {
-      iconClass: styles.appsIcon__iconGooglePlay,
-      href: 'https://play.google.com/store/apps/details?id=ru.dns.shop.android',
-    },
-    { iconClass: styles.appsIcon__iconAppGallery, href: 'https://appgallery.huawei.com/#/app/C108656033' },
-    {
-      iconClass: styles.appsIcon__iconRuStore,
-      href: 'https://apps.rustore.ru/app/ru.dns.shop.android?rsm=1&mt_link_id=ieyfw9',
-    },
-    { iconClass: styles.appsIcon__iconAppStore, href: 'https://apps.apple.com/ru/app/dns-shop/id6450819523' },
-  ];
 
   const handleSectionClick = (section: string) => {
     setSelectedSection((prevSection) => (prevSection === section ? null : section));
@@ -87,26 +51,6 @@ const Footer: React.FC<Props> = () => {
 
     setPrevSection(selectedSection);
   }, [selectedSection, prevSection]);
-
-  const renderLinks = (links: Link[]) => {
-    return links.map((link: Link, index: number) => (
-      <li key={index} className={styles.navmenu__linkWrapper}>
-        <a href={link.href} className={styles.navmenu__submenuLink}>
-          {link.title}
-        </a>
-      </li>
-    ));
-  };
-
-  const renderMobileLinks = (links: Link[]) => {
-    return links.map((link: Link, index: number) => (
-      <li key={index} className={styles.mobileMenu__linkWrapper}>
-        <a href={link.href} className={styles.mobileMenu__submenuLink}>
-          {link.title}
-        </a>
-      </li>
-    ));
-  };
 
   return (
     <footer className={styles.baseFooter}>
