8a6338068df2fc916d5bac020cf2a4fdcaf9ee50
Dmitrii Bolokhontsev <bolokhontsev.main@gmail.com>
2024-05-03 12:42:01 +0300

    merge two components in one

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,19 @@
 import Product from 'entities/product';
 import '../shared/main.css';
 import './index.scss';
+import { useState } from 'react';
 
 const App = () => {
-  return <Product />;
+  const [isHorizontal, setIsHorizontal] = useState<boolean>(true);
+
+  return (
+    <>
+      <button type='button' onClick={() => setIsHorizontal((prev) => !prev)}>
+        ChangeLayout
+      </button>
+      <Product isHorizontal={isHorizontal} vobler={{ text: 'Big long text for vobler', color: '#000' }} />
+    </>
+  );
 };
 
 export default App;
--- src/entities/product/index.tsx
+++ src/entities/product/index.tsx
@@ -1,29 +1,186 @@
-import { FC, useState } from 'react';
-import ProductHorizontal from './product-horizontal';
-import ProductVertical from './product-vertical';
+import { FC, ReactNode, useState } from 'react';
+import horizontalStyles from './product-horizontal.module.scss';
+import verticalStyles from './product-vertical.module.scss';
+import { rateListStyles } from './types';
 import './variables.css';
 
-const Product: FC = () => {
-  const [isSimple, setIsSimple] = useState<boolean>(true);
+interface ProductProps {
+  id?: string | number;
+  photo?: string;
+  name?: string;
+  isHorizontal?: boolean;
+  vobler?: {
+    text: string;
+    color?: string;
+  };
+  statistic?: {
+    rate: number;
+    reviews: number;
+    comments: number;
+  };
+  price?: {
+    currentPrice: number;
+    prevPrice?: number;
+    creditPrice?: number;
+  };
+  avails?: {
+    shops: number;
+    pvz: boolean;
+    delivery: string;
+  };
+}
 
-  if (isSimple) {
-    return (
-      <>
-        <button type='button' onClick={() => setIsSimple(false)}>
-          ChangeLayout
-        </button>
-        <ProductHorizontal />
-      </>
-    );
+const Product: FC<ProductProps> = ({
+  id = 5402341,
+  photo = 'https://www.electrogor.ru/img/work/nomencl/m_121357.jpg',
+  name = '10.2" Планшет Apple iPad (9th Gen) Wi-Fi 64 ГБ серый [2160x1620, IPS, 6x2.66 ГГц, 3 ГБ, 8686 мА*ч, iPadOS 15]',
+  isHorizontal = true,
+  vobler = { text: 'Sale', color: '#ff3322' },
+  statistic = { rate: 4.5, reviews: 1244, comments: 23 },
+  price = { currentPrice: 37456, prevPrice: 0, creditPrice: 2333 },
+  avails = { shops: 25, pvz: true, delivery: 'за 4 часа' },
+}) => {
+  const [isMouseOvered, setIsMouseOvered] = useState<boolean>(false);
+  const [isLiked, setIsLiked] = useState<boolean>(false);
+  const styles = isHorizontal ? horizontalStyles : verticalStyles;
+  const { star, halfStar, emptyStar } = styles;
+  const rateStars: ReactNode[] = rateList(statistic.rate, { star, halfStar, emptyStar });
+
+  function rateList(rate: number, styles: rateListStyles): ReactNode[] {
+    const rateStars: ReactNode[] = [];
+    const { star, halfStar, emptyStar } = styles;
+    let rateCopy = rate;
+
+    for (let indx = 0; indx < 5; indx++) {
+      const styleName = rateCopy >= 1 ? star : rateCopy >= 0.5 ? halfStar : emptyStar;
+
+      const starElement = <i key={indx} className={styleName}></i>;
+
+      rateStars.push(starElement);
+      rateCopy -= 1;
+    }
+    return rateStars;
   }
 
-  return (
+  const productCode = (
+    <div className={isMouseOvered ? styles.image__code : styles.none}>
+      <i></i>&nbsp;{id}
+    </div>
+  );
+
+  const extendStatistics = (
     <>
-      <button type='button' onClick={() => setIsSimple(true)}>
-        ChangeLayout
-      </button>
-      <ProductVertical />
+      {' '}
+      <a className={styles.comment} href='/'>
+        <i></i>&nbsp;{statistic.comments}
+      </a>
+      <a className={styles.service} href='/'>
+        <i></i>Отличная надежность
+      </a>
     </>
+  );
+
+  const shopsBlock = (
+    <div>
+      <span>В наличии </span>
+      <a href='/' className={styles.avails__link}>
+        в {avails.shops} магазинах
+      </a>
+    </div>
+  );
+
+  const pvzBlock = (
+    <div>
+      <span>Пункты выдачи </span>
+      <a href='/' className={styles.avails__link}>
+        доступны {avails.pvz}
+      </a>
+    </div>
+  );
+
+  const deliveryBlock = (
+    <div>
+      <span>Доставим на дом </span>
+      <a href='/' className={styles.avails__link}>
+        {avails.delivery}
+      </a>
+    </div>
+  );
+
+  return (
+    <section
+      className={styles.product}
+      onMouseOver={() => setIsMouseOvered(true)}
+      onFocus={() => setIsMouseOvered(true)}
+      onMouseOut={() => setIsMouseOvered(false)}
+      onBlur={() => setIsMouseOvered(false)}
+    >
+      <div className={styles.image}>
+        <a href='/' className={styles.image__link}>
+          <picture>
+            <img className={styles.image__picture} alt='product' src={photo} />
+          </picture>
+          <i className={isMouseOvered ? styles.image__zoom : undefined}></i>
+        </a>
+        <span className={styles.image__discount}>-10%</span>
+        {isHorizontal ? productCode : null}
+      </div>
+      <a href='/' className={styles.name}>
+        <span className={styles.name__span}>{name}</span>
+      </a>
+      <div className={styles.vobler}>
+        <style>
+          {`#vobler{
+            color: ${vobler.color};
+          }
+          #vobler:after {
+            background-color: ${vobler.color};
+          }`}
+        </style>
+        <div className={styles.vobler__name} id='vobler' title={vobler.text}>
+          {vobler.text}
+        </div>
+      </div>
+      <div className={styles.statistic}>
+        <span className={styles.compareCheckbox}>
+          <label className={styles.compareCheckbox__label}>
+            <span>Сравнить</span>
+            <input type='checkbox' className={styles.compareCheckbox__input} />
+            <span className={styles.compareCheckbox__box}></span>
+          </label>
+        </span>
+        <a className={styles.rating} href='/'>
+          {rateStars}
+          {statistic.reviews}
+        </a>
+        {isHorizontal ? extendStatistics : null}
+      </div>
+      <div className={styles.buy}>
+        <div className={styles.buy__wrap}>
+          <div className={price.prevPrice ? `${styles.buy__price} ${styles.buy__sale}` : styles.buy__price}>
+            {`${price.currentPrice} ₽`}
+            <span className={styles.buy__prev}>{price.prevPrice ? price.prevPrice : null}</span>
+          </div>
+          <div className={styles.buy__hint}></div>
+          <div className={styles.buy__sub}>{price.creditPrice ? `или ${price.creditPrice} ₽/ мес.` : null}</div>
+        </div>
+        <button
+          type='button'
+          data-tooltip='Добавить в избранное'
+          onClick={() => setIsLiked((prev) => !prev)}
+          className={isLiked ? `${styles.wishlist} ${styles.liked}` : styles.wishlist}
+        ></button>
+        <button type='button' className={`${styles.cart} ${isMouseOvered ? styles.cartOnMouseover : undefined}`}>
+          {isHorizontal ? 'купить' : null}
+        </button>
+      </div>
+      <span className={styles.avails}>
+        {avails.shops ? shopsBlock : null}
+        {avails.pvz ? pvzBlock : null}
+        {avails.delivery ? deliveryBlock : null}
+        {!avails.shops && !avails.pvz && !avails.delivery ? 'Товара нет в наличии' : null}
+      </span>
+    </section>
   );
 };
 
