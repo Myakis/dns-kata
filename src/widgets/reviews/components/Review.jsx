61afa355c7a234e5fb6ededf75526902ada8f777
Tsurkan Georgiy <egorizz@mail.ru>
2024-05-03 06:33:20 +0500

    Перенес компонент рейтинга в shared

--- src/widgets/reviews/components/Review.jsx
+++ src/widgets/reviews/components/Review.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from '../reviews.module.scss';
-import StarRating from './StarRating';
+import StarRating from '../../../shared/StarRating';
 
 const Review = ({ reviews, loading }) => {
   if (loading) {
