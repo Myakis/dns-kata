114bc3895c5877872f9b09c89cb52f23166e3261
AsukaTundra <c.vodopyanov@mail.ru>
2024-05-06 19:47:55 +0300

    shere-tooltip off/on

--- src/features/share-tooltip/index.tsx
+++ src/features/share-tooltip/index.tsx
@@ -4,39 +4,62 @@ import { useState } from 'react';
 import style from './style.module.scss';
 
 const ShareTooltip: React.FC = () => {
-  const [panel, setPanel] = useState(false);
+  const [shareModal, setShareModal] = useState(false);
+
+  const clickButtonShare = (): void => {
+    if (!shareModal) {
+      const eventShareModal = (): void => {
+        const event = (): void => {
+          setShareModal(false);
+          document.body.removeEventListener('click', event);
+        };
+
+        document.body.addEventListener('click', event);
+      };
+
+      setShareModal(true);
+      if (window.innerWidth > 991) {
+        setTimeout(() => eventShareModal(), 100);
+      }
+    }
+    return;
+  };
 
   return (
     <div className={style['share']}>
-      <button
-        className={`${style['share__button']} ${style['icon']}`}
-        onClick={() => setPanel((panel) => !panel)}
-      ></button>
-      {panel && (
-        <div className={style['share__block']}>
-          <ul className={style['share__list']}>
-            <li className={style['share__item']}>
-              <a className={`${style['share__link']} ${style['share__link_vk']}`} href='#'>
-                Вконтакте
-              </a>
-            </li>
-            <li className={style['share__item']}>
-              <a className={`${style['share__link']} ${style['share__link-ok']}`} href='#'>
-                Одноклассники
-              </a>
-            </li>
-            <li className={style['share__item']}>
-              <a className={`${style['share__link']} ${style['share__link-copy']}`} href='#'>
-                Скопировать ссылку
-              </a>
-            </li>
-            <li className={style['share__item']}>
-              <a className={`${style['share__link']} ${style['share__link-other']}`} href='#'>
-                Другие способы
-              </a>
-            </li>
-          </ul>
-        </div>
+      <button className={`${style['share__button']} ${style['icon']}`} onClick={clickButtonShare}></button>
+      {shareModal && (
+        <>
+          <div className={style['mobile__background']} onClick={() => setShareModal(false)}></div>
+          <div className={style['share__block']}>
+            <div className={style['share__mobile']}>
+              <p className={style['share__mobile--title']}>Поделиться</p>
+              <button className={style['shere__mobile--button']} onClick={() => setShareModal(false)}></button>
+            </div>
+            <ul className={style['share__list']}>
+              <li className={style['share__item']}>
+                <a className={`${style['share__link']} ${style['share__link_vk']}`} href='#'>
+                  Вконтакте
+                </a>
+              </li>
+              <li className={style['share__item']}>
+                <a className={`${style['share__link']} ${style['share__link-ok']}`} href='#'>
+                  Одноклассники
+                </a>
+              </li>
+              <li className={style['share__item']}>
+                <a className={`${style['share__link']} ${style['share__link-copy']}`} href='#'>
+                  Скопировать ссылку
+                </a>
+              </li>
+              <li className={style['share__item']}>
+                <a className={`${style['share__link']} ${style['share__link-other']}`} href='#'>
+                  Другие способы
+                </a>
+              </li>
+            </ul>
+          </div>
+        </>
       )}
     </div>
   );
