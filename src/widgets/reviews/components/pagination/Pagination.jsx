631c9781c8608105998831bda527f19a46e8bbbd
Tsurkan Georgiy <egorizz@mail.ru>
2024-05-15 00:48:50 +0500

    Доделал пагинацию

--- src/widgets/reviews/components/pagination/Pagination.jsx
+++ src/widgets/reviews/components/pagination/Pagination.jsx
@@ -3,48 +3,71 @@ import PropTypes from 'prop-types';
 import React, { useState } from 'react';
 
 const Pagination = ({ reviewsPerPage, totalReviews, paginate }) => {
-  const [activePage, setActivePage] = useState(1); // Состояние для отслеживания активной страницы
+  const [activePage, setActivePage] = useState(1);
+  const [firstSlice, setFirstSlice] = useState(0);
+  const [lastSlice, setLastSlice] = useState(8);
   const pageNumbers = [];
 
   for (let i = 1; i <= Math.ceil(totalReviews / reviewsPerPage); i++) {
     pageNumbers.push(i);
   }
 
-  const handleClick = (number) => {
-    setActivePage(number); // Устанавливаем активную страницу при клике
+  const updatePagination = (number) => {
+    setActivePage(number);
     paginate(number);
+    if (number > 4 && lastSlice !== pageNumbers.length) {
+      setFirstSlice((prev) => prev + 1);
+      setLastSlice((prev) => prev + 1);
+    } else if (number < 6 && firstSlice !== 0) {
+      setFirstSlice((prev) => prev - 1);
+      setLastSlice((prev) => prev - 1);
+    }
+  };
+
+  const handleClick = (number) => {
+    updatePagination(number);
   };
 
   const nextPage = () => {
-    const next = activePage + 1; // Получаем номер следующей страницы
+    const next = activePage + 1;
 
     if (next <= pageNumbers.length) {
-      // Проверяем, что следующая страница существует
-      setActivePage(next); // Устанавливаем следующую страницу как активную
-      paginate(next); // Вызываем функцию paginate для перехода на следующую страницу
+      setActivePage(next);
+      paginate(next);
+      if (next > 4 && lastSlice !== pageNumbers.length) {
+        setFirstSlice((prev) => prev + 1);
+        setLastSlice((prev) => prev + 1);
+      }
     }
   };
 
   const prevPage = () => {
-    const prev = activePage - 1; // Получаем номер предыдущей страницы
+    const prev = activePage - 1;
 
     if (prev >= 1) {
-      // Проверяем, что предыдущая страница существует
-      setActivePage(prev); // Устанавливаем предыдущую страницу как активную
-      paginate(prev); // Вызываем функцию paginate для перехода на предыдущую страницу
+      setActivePage(prev);
+      paginate(prev);
+      if (prev < 6 && firstSlice !== 0) {
+        setFirstSlice((prev) => prev - 1);
+        setLastSlice((prev) => prev - 1);
+      }
     }
   };
 
   const firstPage = () => {
     setActivePage(1);
     paginate(1);
+    setFirstSlice(0);
+    setLastSlice(8);
   };
 
   const lastPage = () => {
-    const last = Math.ceil(totalReviews / reviewsPerPage); // Получаем номер последней страницы
+    const last = Math.ceil(totalReviews / reviewsPerPage);
 
-    setActivePage(last); // Устанавливаем последнюю страницу как активную
-    paginate(last); // Вызываем функцию paginate для перехода на последнюю страницу
+    setActivePage(last);
+    paginate(last);
+    setFirstSlice(last - 8);
+    setLastSlice(last);
   };
 
   return (
@@ -58,9 +81,9 @@ const Pagination = ({ reviewsPerPage, totalReviews, paginate }) => {
         </div>
         <ul
           className={styles.paginatorWidget__pagesList}
-          style={{ flexBasis: `${Math.ceil(totalReviews / reviewsPerPage) * 65}px` }}
+          style={{ flexBasis: `${Math.ceil(totalReviews / reviewsPerPage) * 52}px` }}
         >
-          {pageNumbers.map((number) => (
+          {pageNumbers.slice(firstSlice, lastSlice).map((number) => (
             <li key={number}>
               <div
                 className={`${styles.paginatorWidget__page} ${activePage === number ? styles.paginatorWidget__page_active : ''}`}
